1e64b634f17db9df87fd7b1b5acd2c3eddbfbca4
kli561 <kevinjli816@gmail.com>
2020-04-03 16:57:33 -0400

    need to fix loop for running summary stats per each chem parameter

--- 5YrRpt.R
+++ 5YrRpt.R
@@ -209,6 +209,75 @@ num.sbasn.over1<-count(sbasn.sample.frequency, total > 1)
 
 "percentage of subregional basins with more than one sample"<-print(100 * (num.sbasn.over1$n / length(unique(sbasn.df$sbasn))))  
   
+---------------------------------------------
+
+##What are the summary statistics for each parameter? (edit)
+
+  #extract dfs for individual chemparameter
+Magnesium<-chem_basin[chem_basin$chemparameter=="Magnesium"&chem_basin$duplicate==0,]
+Nitrite<-chem_basin[chem_basin$chemparameter=="Nitrite"&chem_basin$duplicate==0,]
+Nitrate<-chem_basin[chem_basin$chemparameter=="Nitrate"&chem_basin$duplicate==0,]
+Ammonia<-chem_basin[chem_basin$chemparameter=="Ammonia"&chem_basin$duplicate==0,]
+Calcium<-chem_basin[chem_basin$chemparameter=="Calcium"&chem_basin$duplicate==0,]
+Hardness<-chem_basin[chem_basin$chemparameter=="Hardness"&chem_basin$duplicate==0,]
+"Organic Nitrogen"<-chem_basin[chem_basin$chemparameter=="Organic Nitrogen"&chem_basin$duplicate==0,]
+"Ortho Phosphate"<-chem_basin[chem_basin$chemparameter=="Ortho Phosphate"&chem_basin$duplicate==0,]
+"pH"<-chem_basin[chem_basin$chemparameter=="pH"&chem_basin$duplicate==0,]
+"Total Kjeldahl Nitrogen"<-chem_basin[chem_basin$chemparameter=="Total Kjeldahl Nitrogen"&chem_basin$duplicate==0,]
+"Total Nitrogen"<-chem_basin[chem_basin$chemparameter=="Total Nitrogen"&chem_basin$duplicate==0,]
+"Total Solids"<-chem_basin[chem_basin$chemparameter=="Total Solids"&chem_basin$duplicate==0,]
+"Total Suspended Solids"<-chem_basin[chem_basin$chemparameter=="Total Suspended Solids"&chem_basin$duplicate==0,]
+Turbidity<-chem_basin[chem_basin$chemparameter=="Turbidity"&chem_basin$duplicate==0,]
+Alkalinity<-chem_basin[chem_basin$chemparameter=="Alkalinity"&chem_basin$duplicate==0,]
+"Total Phosphorous"<-chem_basin[chem_basin$chemparameter=="Total Phosphorous"&chem_basin$duplicate==0,]
+Chloride<-chem_basin[chem_basin$chemparameter=="Chloride"&chem_basin$duplicate==0,]
+"Total Organic Carbon"<-chem_basin[chem_basin$chemparameter=="Total Organic Carbon"&chem_basin$duplicate==0,]
+Silica<-chem_basin[chem_basin$chemparameter=="Silica"&chem_basin$duplicate==0,]
+"Chlorophyll-a Plankton"<-chem_basin[chem_basin$chemparameter=="Chlorophyll-a Plankton"&chem_basin$duplicate==0,]
+"Nitrate/Nitrite"<-chem_basin[chem_basin$chemparameter=="Nitrate/Nitrite"&chem_basin$duplicate==0,]
+
+  #create list that stores dfs
+list_parameters<-list(Magnesium, 
+                      Nitrate, 
+                      Nitrite, 
+                      Ammonia, 
+                      Calcium, 
+                      Hardness, 
+                      `Organic Nitrogen`,
+                      `Ortho Phosphate`,
+                      pH,
+                      `Total Kjeldahl Nitrogen`,
+                      `Total Nitrogen`,
+                      `Total Solids`,
+                      `Total Suspended Solids`,
+                      Turbidity,
+                      Alkalinity,
+                      `Total Phosphorous`,
+                      Chloride,
+                      `Total Organic Carbon`,
+                      Silica,
+                      `Chlorophyll-a Plankton`,
+                      `Nitrate/Nitrite`)
+
+library(data.table)
+
+for (i in seq_along(list_parameters)) list_parameters[[i]]$chemparameter <- names(list_parameters)[i]
+
+  #convert list into data frame
+DT.parameters <- rbindlist(list_parameters)
+  #subset for parameter name and value
+DT.parameters <-subset(DT.parameters, select = c("chemparameter","value"))
+
+for (i in 1:length(chemunique)) {
+  chem.i<-chemunique[i]
+  parametersumm<-summary(DT.parameters[DT.parameters$chemparameter==chem.i&DT.parameters$duplicate==0,])
+  print(parametersumm)
+}  
   
+##Only calculate summary statistics for river/stream samples and non-duplicates
   
+##What are the summary statistics for each parameter in each major basin?
   
+##what are the summary stats for drainage area for the sites? By major basin?
+  
+##What are the summary stats for percent impervious cover for the sites? By major basin?
