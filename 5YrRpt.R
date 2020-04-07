a09a8ea5c3b6c7fdf829979748ac8be6a5d157a0
kli561 <kevinjli816@gmail.com>
2020-04-07 16:25:37 -0400

    Still trying to make summary stats table work for me

--- 5YrRpt.R
+++ 5YrRpt.R
@@ -48,13 +48,6 @@ chem_basin<-merge(chem1,sitesbasin,by="sta_seq")
 
 parametername<-chem[4]  
 --------------------------
-  #Parse chemparameter data:
-  
-  #parse out data by row for unique chem parameters
-for (i in 1:length(chemunique)) {
-  chem = chemunique[i]
-  chem_total <-chem_basin[chem_basin$chemparameter==chem & chem_basin$duplicate==0, ]
-}
 ---------------------------
 ##what chemical parameters were collected?
   
@@ -214,74 +207,18 @@ num.sbasn.over1<-count(sbasn.sample.frequency, total > 1)
 ##What are the summary statistics for each parameter? (edit)
 
   #extract dfs for individual chemparameter
-Magnesium<-chem_basin[chem_basin$chemparameter=="Magnesium"&chem_basin$duplicate==0,]
-Nitrite<-chem_basin[chem_basin$chemparameter=="Nitrite"&chem_basin$duplicate==0,]
-Nitrate<-chem_basin[chem_basin$chemparameter=="Nitrate"&chem_basin$duplicate==0,]
-Ammonia<-chem_basin[chem_basin$chemparameter=="Ammonia"&chem_basin$duplicate==0,]
-Calcium<-chem_basin[chem_basin$chemparameter=="Calcium"&chem_basin$duplicate==0,]
-Hardness<-chem_basin[chem_basin$chemparameter=="Hardness"&chem_basin$duplicate==0,]
-"Organic Nitrogen"<-chem_basin[chem_basin$chemparameter=="Organic Nitrogen"&chem_basin$duplicate==0,]
-"Ortho Phosphate"<-chem_basin[chem_basin$chemparameter=="Ortho Phosphate"&chem_basin$duplicate==0,]
-"pH"<-chem_basin[chem_basin$chemparameter=="pH"&chem_basin$duplicate==0,]
-"Total Kjeldahl Nitrogen"<-chem_basin[chem_basin$chemparameter=="Total Kjeldahl Nitrogen"&chem_basin$duplicate==0,]
-"Total Nitrogen"<-chem_basin[chem_basin$chemparameter=="Total Nitrogen"&chem_basin$duplicate==0,]
-"Total Solids"<-chem_basin[chem_basin$chemparameter=="Total Solids"&chem_basin$duplicate==0,]
-"Total Suspended Solids"<-chem_basin[chem_basin$chemparameter=="Total Suspended Solids"&chem_basin$duplicate==0,]
-Turbidity<-chem_basin[chem_basin$chemparameter=="Turbidity"&chem_basin$duplicate==0,]
-Alkalinity<-chem_basin[chem_basin$chemparameter=="Alkalinity"&chem_basin$duplicate==0,]
-"Total Phosphorous"<-chem_basin[chem_basin$chemparameter=="Total Phosphorous"&chem_basin$duplicate==0,]
-Chloride<-chem_basin[chem_basin$chemparameter=="Chloride"&chem_basin$duplicate==0,]
-"Total Organic Carbon"<-chem_basin[chem_basin$chemparameter=="Total Organic Carbon"&chem_basin$duplicate==0,]
-Silica<-chem_basin[chem_basin$chemparameter=="Silica"&chem_basin$duplicate==0,]
-"Chlorophyll-a Plankton"<-chem_basin[chem_basin$chemparameter=="Chlorophyll-a Plankton"&chem_basin$duplicate==0,]
-"Nitrate/Nitrite"<-chem_basin[chem_basin$chemparameter=="Nitrate/Nitrite"&chem_basin$duplicate==0,]
+DT.parameters1 <-subset(chem_basin, select = c("chemparameter","value"))
+split.parameter1<-split(DT.parameters1,DT.parameters1$chemparameter)
 
-  #create list that stores dfs
-list_parameters<-list(Magnesium, 
-                      Nitrate, 
-                      Nitrite, 
-                      Ammonia, 
-                      Calcium, 
-                      Hardness, 
-                      `Organic Nitrogen`,
-                      `Ortho Phosphate`,
-                      pH,
-                      `Total Kjeldahl Nitrogen`,
-                      `Total Nitrogen`,
-                      `Total Solids`,
-                      `Total Suspended Solids`,
-                      Turbidity,
-                      Alkalinity,
-                      `Total Phosphorous`,
-                      Chloride,
-                      `Total Organic Carbon`,
-                      Silica,
-                      `Chlorophyll-a Plankton`,
-                      `Nitrate/Nitrite`)
-
-library(data.table)
-
-for (i in seq_along(list_parameters)) list_parameters[[i]]$chemparameter <- names(list_parameters)[i]
-
-  #convert list into data frame
-DT.parameters <- rbindlist(list_parameters)
-  #subset for parameter name and value
-DT.parameters <-subset(DT.parameters, select = c("chemparameter","value"))
-
-chemunique
-col<-match(DT.parameters$chemparameter, chemunique)
-row<-cumsum(c(0,diff(col))<=0)
-
-Param_summ <- matrix(nrow=max(row), ncol=max(col))
-colnames(Param_summ) <- chemunique
-Param_summ[cbind(row, col)] <- DT.parameters$value
+for (i in 1:length(chemunique)) {
+  parami<-chemunique[i]
+  
+  lapply(seq_along(split.parameter1), 
+         function(i,x) {assign(paste0(chemunique[i]),x[[i]], envir=.GlobalEnv)},
+         x=split.parameter1)
+}
 
 
-
-library(pastecs)
-options(scipen = 100)
-options(digits = 2)
-stat.desc(Param_summ)
   
 ##Only calculate summary statistics for river/stream samples and non-duplicates
   
