da8eba6a0b197acd07ace7fdb00d91dd1f257280
kli561 <kevinjli816@gmail.com>
2020-04-29 14:33:55 -0400

    cleaning code 4/29

--- 5YrRpt.R
+++ 5YrRpt.R
@@ -1,4 +1,10 @@
 library('RSQLite')
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(scales)
+library(tidyr)
+
 setwd("C:/Users/kevin/Documents/Projects/GitHub/DEEP QA/5YrMonitoringRpt")
 env<-read.csv("data/site_env.csv",header=TRUE)
 db_path <- paste0(getwd(),'/data/')
@@ -39,22 +45,26 @@ chemMdl<-merge(chem1,mdl,by="chemparameter",all.x=TRUE)
 chemMdl$valueMDL<-ifelse(is.na(chemMdl$value),chemMdl$MDL,chemMdl$value)
 
 chem_basin<-merge(chem1,sitesbasin,by="sta_seq")
---------------------------
+
+
 #vector of unique chemical parameters
 uniquechem<-unique(chem_basin$chemparameter)
----------------------------
+
+
 ##what chemical parameters were collected?
   
 uniquechem<-unique(chem_basin$chemparameter)
 ChemParam<-data.frame(uniquechem)
 
----------------------------
+
+
 ##How many river and stream sites were samples from 2011 through 2015? (edit)
   
 NumberSites<-length(chem_basin$collect_date)
 Number_of_sites<-data.frame(NumberSites)
 
----------------------------
+
+
 ##How many samples were collected for each chemical parameter at river/stream sites? 
   
   #create vector of unique chem parameters
@@ -78,9 +88,9 @@ for (i in 1:length(chemunique)){
   #create df*
 Parameter_Count<-data.frame(ChemSamp)
 
-----------------------------------------
+
+
 ##Do all of the samples have the same unit of measure for each given parameter?
-library(dplyr)
   
   #subset table 
 uom_chemparameter<-chem_basin[,c("chemparameter","uom")]
@@ -94,16 +104,17 @@ inner_join(uom_chemparameter, unique_uom)
 
   #identify rows in uom_chemparameter that are not present in unique_uom (dplyr)*
 error_uom<-data.frame(setdiff(uom_chemparameter, unique_uom))
-----------------------------------------
+
+
 ##How many river and stream samples were collected excluding duplicates?
   #exclude duplicates
 nodup_chem<-distinct(chem_basin,.keep_all=TRUE)
 Num_of_sample<-data.frame(length(nodup_chem$station_type))
-----------------------------------------          
+
 ##How many river and stream samples (excluding duplicates) were collected in each year? (edit)
 
   #create df for dates
-library(tidyverse)
+
 dates <- data.frame(chem_basin[2], stringsAsFactors = FALSE)
 
   #separate dates into "month", "day", "year"
@@ -112,10 +123,9 @@ monthdayyear<-data.frame(separate(dates,"collect_date", c("month", "day", "year"
   #create freq. table for sample years
 sampleyearfreq<-as.data.frame(table(data.frame(monthdayyear[3])))
   colnames(sampleyear)[1] <- "Year"
-----------------------------------------
+
+  
 ##How many sites were collected in more than one year? ##What sites were collected in more than one year and how many years were collected?
-require(tidyverse)
-require(dplyr)
   
   #extract "year" from collection date
 sampleyear<-
@@ -139,7 +149,8 @@ morethanone<- sampleyear.freq %>%
   
   #number of sites sampled in more than one year*
 length(morethanone$sta_seq)
-------------------------------------------
+
+
 ##How many sites were collected in each major basin?
   #subset chem_basin
 table.major.basin<-
@@ -152,9 +163,10 @@ table.major.basin<-cbind(sta_seq = row.names(table.major.basin),
   #sum, exclude sta_seq
 sites.per.mbasin<-data.frame(colSums(table.major.basin[,-1]))
   colnames(sites.per.mbasin)[1] <- "Site Frequency"
--------------------------------------------  
+
+   
 ## What percentage of subregional basins in the State have one or more samples? (edit)
-    require(dplyr)
+
   #create frequency table
 sbasn<-table(subset(chem_basin, select = c("sbasn", "sta_seq")))
   #sbasn as column 1, convert to matrix df
@@ -164,7 +176,8 @@ sbasn.total<-data.frame(cbind(sbasn.total, total = rowSums(sbasn.total[-1])))
 sbasn.sample.frequency<-count(sbasn.total, total > 1)
   
 "percentage of subregional basins with more than one sample"<-print(100 * (sbasn.sample.frequency$n / length(unique(sbasn.total$sbasn))))     
----------------------------------------------
+
+
 ##What are the summary statistics for each parameter? ##Only calculate summary statistics for river/stream samples and non-duplicates
   #Create a summary stats dataframe
 summary_Stats<-data.frame(param=character(),Min=numeric(),Q1 =numeric(),Median=numeric(),
@@ -180,12 +193,12 @@ for (i in 1:length(uniquechem)){
                    Mean=test[4,],Q3=test[5,],Max=test[6,],NAs=cntNA)
   summary_Stats<-rbind(test,summary_Stats)
 }  
----------------------------------------------  
+
+
 ##What are the summary statistics for each parameter in each major basin? (edit)
   #aggregate values based on major basin & parameter
 mbasin.para<-aggregate(chem_basin$value , by = list(chem_basin$major , chem_basin$chemparameter )  , FUN = summary)  
 
-require(dplyr)
 
   #rename columns
 mbasin.para<- mbasin.para %>% 
@@ -196,11 +209,10 @@ mbasin.para<- mbasin.para %>%
   )
   #sort columns by major basin
 mbasin.para<-data.frame(arrange(mbasin.para,MajorBasin))
----------------------------------------------  
+
+
 ##what are the summary stats for drainage area for the sites? By major basin?
   
-require(dplyr)
-
 StationMajor<-
   data.frame(subset(distinct(chem_basin,sta_seq, .keep_all = TRUE), 
                     select = c("sta_seq","major")))
@@ -225,7 +237,8 @@ Drainagearea<-as.data.frame(Majorenv %>%
                                                                          max = "max",
                                                                          mean = "mean", 
                                                                          sd = "sd")))
-----------------------------------------------
+
+
 ##What are the summary stats for percent impervious cover for the sites? By major basin?
 
   #summary stats percent impervious cover
@@ -244,7 +257,8 @@ PercentIC<-as.data.frame(Majorenv.IC %>%
                                                                          max = "max",
                                                                          mean = "mean", 
                                                                          sd = "sd")))
------------------------------------------------
+
+
 #Table Major Basin - Chem Parameter
 
   #standard error function
@@ -256,9 +270,9 @@ mbasin.para1<-as.data.frame(mbasin.para1 %>%
                               group_by(major,chemparameter) %>%
                               summarise_at(.vars = names(.)[3],.funs = c(count = "length",
                                                                          min = "min",
-                                                                         q1 = ~quantile(.,probs = 0.25, na.rm = T), 
+                                                                         q1 = ~quantile(.,probs = 0.25, na.rm = TRUE), 
                                                                          median = "median", 
-                                                                         q2 = ~quantile(.,probs = 0.75, na.rm = T), 
+                                                                         q2 = ~quantile(.,probs = 0.75, na.rm = TRUE), 
                                                                          max = "max",
                                                                          mean = "mean", 
                                                                          sd = "sd",
@@ -270,9 +284,9 @@ Statewide <- as.data.frame(omitna.chembasin %>%
                              group_by(chemparameter) %>%
                              summarise_at(.vars = names(.)[4],.funs = c(count = "length",
                                                                         min = "min",
-                                                                        q1 = ~quantile(.,probs = 0.25, na.rm = T), 
+                                                                        q1 = ~quantile(.,probs = 0.25, na.rm = TRUE), 
                                                                         median = "median", 
-                                                                        q2 = ~quantile(.,probs = 0.75, na.rm = T), 
+                                                                        q2 = ~quantile(.,probs = 0.75, na.rm = TRUE), 
                                                                         max = "max",
                                                                         mean = "mean", 
                                                                         sd = "sd",
@@ -298,9 +312,10 @@ chloride <-data.frame(cbind(chloride, chloride.state))
 names(chloride)[names(chloride) == 'chloride.state'] <- 'Statewide'
 
 
-
   #boxplot Chloride
+
 library(ggplot2)
+library(scales)
 
 chloride2 <- data.frame(na.omit(subset(chem_basin, select = c("chemparameter","value"))))
 chloride2 <- data.frame(subset(chloride2, chloride2$chemparameter == "Chloride"))
@@ -339,7 +354,7 @@ p <- ggplot(chloride1, aes(x= major, y= value, fill = major)) +
   scale_fill_brewer(palette="RdBu")+
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5))+
-  stat_summary(fun = mean, geom="point", shape=23, size=4)+
+  stat_summary(fun = mean, geom="point", shape=23, size=3)+
   theme(axis.text.x = element_text(angle=90, hjust = 1))+
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x)))+
